b812ce1c0348e767d8e5f388ebdde614d374cdbd
dave <dave@fedora.home>
2016-07-06 00:56:06 +0100

    first push, probs broken...cant test

---
+++ commands.cpp
@@ -0,0 +1,30 @@
+#include "tokenscanner.h"
+#include <iostream>
+
+int main () {
+  std::cout << "Loaded command scanner.";
+  return 0;
+}
+
+void inputStrip(line) {
+  TokenScanner scanner;
+  scanner.setInput(input);
+  string number = scanner.nextToken();
+  if (scanner.getTokenType(number) == NUMBER) 
+    {
+      int line_number = stringToInteger(firstTerm);
+    {
+      else 
+	{
+	  
+      string token = scanner.nextToken();
+	}
+
+    }
+
+    }
+
+}
+
+
+  
